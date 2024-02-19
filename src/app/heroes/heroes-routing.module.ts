8c7421ad3b5ab260d888f7cd1a2fddafe65ad091
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-19 08:30:47 +0100

    S13 - Paths de las rutas faltantes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent,
     children: [
-      {
-        path: 'new-hero',
-        component: NewPageComponent,
-      }
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: ':id', component: HeroPageComponent },
+      //! La ruta :id debe escribirse la última porque id en este caso es un
+      //! comodín, por lo que si se hubiese puesto la primera, nunca entraría
+      //! en las demás rutas
+      { path: '**', redirectTo: 'list' },
+      //? No habíamos quedado en que :id era un comodín? Entonces, cuándo
+      //? entraría en la ruta comodín?
+      //* Entraría por ejemplo, en el caso de que el path sea ''
     ]
   }
 ];
